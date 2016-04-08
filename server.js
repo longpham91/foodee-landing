d481a2d2b200d8e93e94c5917b560d83aeca072d
Long Pham <longpham91@gmail.com>
2016-04-08 16:54:10 -0400

    create repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express      = require('express');
+var app          = express();
+var favicon      = require('serve-favicon');
+var router       = express.Router();
+var bodyParser   = require('body-parser');
+var MessageController = require('./routes/message');
+
+/**
+ * Routing & middlewares
+ */
+app.use(express.static(__dirname + '/static'));
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(bodyParser.json({ extended: true }));
+app.use(router);
+
+router.post('/sendmessage', MessageController.post);
+
+app.use(function (request, response) {
+    response.status(404).json({
+        message: 'Oops! You may get lost!',
+        code: 'E_NOTFOUND'
+    });
+});
+
+app.use(function (error, request, response, next) {
+    response.status(500).json({
+        message: error.message,
+        code: error.code
+    });
+});
+module.exports = app;
