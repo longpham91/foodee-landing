f747ebf1bafcf72741631b657809171ea76253cd
Quoc Nghi <quocnghi@Quocs-MacBook-Pro.local>
2016-04-11 07:43:45 +0700

    move app run to server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express      = require('express');
-var app          = express();
+var express = require('express');
+var app = express();
 // var favicon      = require('serve-favicon');
-var router       = express.Router();
-var bodyParser   = require('body-parser');
+var router = express.Router();
+var bodyParser = require('body-parser');
 var MessageController = require('./routes/message');
 
 /**
@@ -16,16 +16,23 @@ app.use(router);
 router.post('/sendmessage', MessageController.post);
 
 app.use(function (request, response) {
-    response.status(404).json({
-        message: 'Oops! You may get lost!',
-        code: 'E_NOTFOUND'
-    });
+	response.status(404).json({
+		message: 'Oops! You may get lost!',
+		code: 'E_NOTFOUND'
+	});
 });
 
 app.use(function (error, request, response, next) {
-    response.status(500).json({
-        message: error.message,
-        code: error.code
-    });
+	response.status(500).json({
+		message: error.message,
+		code: error.code
+	});
 });
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+	console.log('Foodee service runs on port: ' + app.get('port'));
+});
+
 module.exports = app;
