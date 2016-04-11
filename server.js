3a1d33fc8eabd1d5d8fe1eba60321e27bc740a07
Long Pham <longpham91@gmail.com>
2016-04-11 14:35:27 -0400

    add subscribe route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
 var MessageController = require('./routes/message');
+var SubscribeController = require('./routes/subscribe');
 
 /**
  * Routing & middlewares
@@ -14,6 +15,7 @@ app.use(bodyParser.json({ extended: true }));
 app.use(router);
 
 router.post('/sendmessage', MessageController.post);
+router.post('/subscribe', SubscribeController.post);
 
 app.use(function (request, response) {
 	response.status(404).json({
