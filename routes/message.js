9f7eb56a7f7832c72eb8ad4645ae8c77acce5519
Nghi <nghibui@chotot.vn>
2016-05-02 23:24:46 +0700

    change password for email

--- routes/message.js
+++ routes/message.js
@@ -1,36 +1,36 @@
 var nodemailer = require('nodemailer');
 
-exports.post = function(request, response) {
-    if ((!request.body.name) || (!request.body.email) || (!request.body.phone) || (!request.body.message)) {
-        return response.status(500).json({ success: false, data: 'No arguments provided!'});
-    }
-    
-    var smtpTransport = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // use SSL
-        auth: {
-            user: "contact@foddee.io",
-            pass: "startup101"
-        }
-    });
-    
-    var name = request.body.name;
-    var email = request.body.email;
-    var phone = request.body.phone;
-    var message = request.body.message;
-    
-    var mailOptions = {
-        from: email, // sender address 
-        to: 'contact@foddee.io', // list of receivers 
-        subject: 'Website Contact Form: ' + name, // Subject line
-        html: 'You have received a new message from your website contact form.<br /><br />Here are the details:<br /><br /><strong>Name</strong>: '+ name + '<br /><br /><strong>Email</strong>: ' + email + '<br /><br /><strong>Phone</strong>: ' + phone + '<br /><br /><strong>Message</strong>:<br />' + message
-    };
-    
-    smtpTransport.sendMail(mailOptions, function(error, info){
-        if(error){
-            return response.status(500).json({ success: false, data: error});
-        }
-        response.json({result: 'Message sent: ' + info.response});
-    });
-}
+exports.post = function (request, response) {
+	if ((!request.body.name) || (!request.body.email) || (!request.body.phone) || (!request.body.message)) {
+		return response.status(500).json({ success: false, data: 'No arguments provided!'});
+	}
+
+	var smtpTransport = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true, // use SSL
+		auth: {
+			user: 'contact@foddee.io',
+			pass: 'Honngocviendong91'
+		}
+	});
+
+	var name = request.body.name;
+	var email = request.body.email;
+	var phone = request.body.phone;
+	var message = request.body.message;
+
+	var mailOptions = {
+		from: email, // sender address
+		to: 'contact@foddee.io', // list of receivers
+		subject: 'Website Contact Form: ' + name, // Subject line
+		html: 'You have received a new message from your website contact form.<br /><br />Here are the details:<br /><br /><strong>Name</strong>: ' + name + '<br /><br /><strong>Email</strong>: ' + email + '<br /><br /><strong>Phone</strong>: ' + phone + '<br /><br /><strong>Message</strong>:<br />' + message
+	};
+
+	smtpTransport.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			return response.status(500).json({ success: false, data: error});
+		}
+		response.json({result: 'Message sent: ' + info.response});
+	});
+};
