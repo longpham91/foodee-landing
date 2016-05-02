afb88624d44a0b484a73d8b6073b80ddf76a3cd3
Nghi <nghibui@chotot.vn>
2016-05-03 01:04:44 +0700

    fix email config

--- routes/message.js
+++ routes/message.js
@@ -1,30 +1,20 @@
 var nodemailer = require('nodemailer');
 
 exports.post = function (request, response) {
-	if ((!request.body.name) || (!request.body.email) || (!request.body.phone) || (!request.body.message)) {
+	if ((!request.body.name) || (!request.body.email) || (!request.body.message)) {
 		return response.status(500).json({ success: false, data: 'No arguments provided!'});
 	}
 
-	var smtpTransport = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 465,
-		secure: true, // use SSL
-		auth: {
-			user: 'contact@foddee.io',
-			pass: 'Honngocviendong91'
-		}
-	});
-
+	var smtpTransport = nodemailer.createTransport('smtps://contact%40foodee.io:Honngocviendong91@smtp.gmail.com');
 	var name = request.body.name;
 	var email = request.body.email;
-	var phone = request.body.phone;
 	var message = request.body.message;
 
 	var mailOptions = {
 		from: email, // sender address
-		to: 'contact@foddee.io', // list of receivers
+		to: 'contact@foodee.io', // reciever
 		subject: 'Website Contact Form: ' + name, // Subject line
-		html: 'You have received a new message from your website contact form.<br /><br />Here are the details:<br /><br /><strong>Name</strong>: ' + name + '<br /><br /><strong>Email</strong>: ' + email + '<br /><br /><strong>Phone</strong>: ' + phone + '<br /><br /><strong>Message</strong>:<br />' + message
+		html: 'You have received a new message from your website contact form.<br /><br />Here are the details:<br /><br /><strong>Name</strong>: ' + name + '<br /><br /><strong>Email</strong>: ' + email + '<br /><br /><strong>Message</strong>:<br />' + message
 	};
 
 	smtpTransport.sendMail(mailOptions, function (error, info) {
