94e47488cf9c89848904fb09fd5018119589a3d4
Long Pham <longpham91@gmail.com>
2016-05-10 16:36:36 -0400

    update status text

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -6,7 +6,7 @@ exports.subscribe = function (req, res, next) {
 	var listId = '45785cb28f';
 	mc.lists.subscribe({id: listId, email: {email: req.body.email}}, function (data) {
 		// req.session.success_flash = 'User subscribed successfully! Look for the confirmation email.';
-		return res.json({message: 'Thank you ! You have subscribed to our customer list successfully'});
+		return res.json({message: 'Thank you! You have successfully subscribed to receive latest updates about our launch.'});
 	}, function (error) {
 		if (error) {
 			return res.status(400).json({message: error.error});
