8d349c70ffdcf2089d52a806ba802b96ad4f78da
Long Pham <longpham91@gmail.com>
2016-08-09 17:41:45 +0700

    add randomization to gallery

--- static/js/main.js
+++ static/js/main.js
@@ -241,6 +241,112 @@ $(function () {
    gallery
    ========================================================================== */
 
+	var images = [{
+        url: "img/gallery/01.jpg",
+		mUrl: "img/gallery/m01.jpg",
+		tUrl: "img/gallery/thumbnails/01@2x.jpg",
+        alt: "Calamari",
+		caption: "Calamari<br />The Blue Door Restaurant & Bar<br />San Jose, CA"
+    }, {
+        url: "img/gallery/02.jpg",
+		mUrl: "img/gallery/m02.jpg",
+		tUrl: "img/gallery/thumbnails/02@2x.jpg",
+        alt: "Eggs in Jail",
+		caption: "Eggs in Jail<br />Outerlands<br />San Francisco, CA"
+    }, {
+        url: "img/gallery/03.jpg",
+		mUrl: "img/gallery/m03.jpg",
+		tUrl: "img/gallery/thumbnails/03@2x.jpg",
+        alt: "Spicy Miso Ramen",
+		caption: "Spicy Miso Ramen<br />Niu B Sushi & Robata<br />Chicago, IL"
+    }, {
+        url: "img/gallery/04.jpg",
+		mUrl: "img/gallery/m04.jpg",
+		tUrl: "img/gallery/thumbnails/04@2x.jpg",
+        alt: "Classic Italiano Pizza",
+		caption: "Classic Italiano Pizza<br />Willow Street Wood-Fired Pizza<br />San Jose, CA"
+    }, {
+        url: "img/gallery/05.jpg",
+		mUrl: "img/gallery/m05.jpg",
+		tUrl: "img/gallery/thumbnails/05@2x.jpg",
+        alt: "Pho / Vietnamese Beef Noodle Soup",
+		caption: "Pho / Vietnamese Beef Noodle Soup<br />Pho 90 degree<br />San Jose, CA"
+    }, {
+        url: "img/gallery/06.jpg",
+		mUrl: "img/gallery/m06.jpg",
+		tUrl: "img/gallery/thumbnails/06@2x.jpg",
+        alt: "Truffle Burger",
+		caption: "Truffle Burger<br />Umami Burger<br />Palo Alto, CA"
+    }, {
+        url: "img/gallery/08.jpg",
+		mUrl: "img/gallery/m08.jpg",
+		tUrl: "img/gallery/thumbnails/08@2x.jpg",
+        alt: "Steak Tacos",
+		caption: "Steak Tacos<br />Moctezuma’s Mexican Restaurant<br />Tacoma, WA"
+    }, {
+        url: "img/gallery/09.jpg",
+		mUrl: "img/gallery/m09.jpg",
+		tUrl: "img/gallery/thumbnails/09@2x.jpg",
+        alt: "Unagi (eel) Poke Bowl",
+		caption: "Unagi (eel) Poke Bowl<br />45th Stop N Shop & Poke Bar<br />Seattle, WA"
+    }, {
+        url: "img/gallery/10.jpg",
+		mUrl: "img/gallery/m10.jpg",
+		tUrl: "img/gallery/thumbnails/10@2x.jpg",
+        alt: "Mushroom Hot Pot",
+		caption: "Mushroom Hot Pot<br />Mint and Basil<br />San Jose, CA"
+    }, {
+        url: "img/gallery/11.jpg",
+		mUrl: "img/gallery/m11.jpg",
+		tUrl: "img/gallery/thumbnails/11@2x.jpg",
+        alt: "Sashimi Combo",
+		caption: "Sashimi Combo<br />Yechon<br />Annandale, VA"
+    }, {
+        url: "img/gallery/12.jpg",
+		mUrl: "img/gallery/m12.jpg",
+		tUrl: "img/gallery/thumbnails/12@2x.jpg",
+        alt: "Sticky Rice Mango",
+		caption: "Sticky Rice Mango<br />House of Thai<br />San Francisco, CA"
+    }, {
+        url: "img/gallery/13.jpg",
+		mUrl: "img/gallery/m13.jpg",
+		tUrl: "img/gallery/thumbnails/13@2x.jpg",
+        alt: "Green Tea Mille Crepes",
+		caption: "Green Tea Mille Crepes<br />Lady M Cake Boutique<br />New York, NY"
+    }, {
+        url: "img/gallery/14.jpg",
+		mUrl: "img/gallery/m14.jpg",
+		tUrl: "img/gallery/thumbnails/14@2x.jpg",
+        alt: "Chef’s Tasting",
+		caption: "Chef’s Tasting<br />Dandelion Chocolate<br />San Francisco, CA"
+    }, {
+        url: "img/gallery/17.jpg",
+		mUrl: "img/gallery/m17.jpg",
+		tUrl: "img/gallery/thumbnails/17@2x.jpg",
+        alt: "Acai Bowl",
+		caption: "Acai Bowl<br />Project Juice<br />San Francisco, CA"
+    }, {
+        url: "img/gallery/18.jpg",
+		mUrl: "img/gallery/m18.jpg",
+		tUrl: "img/gallery/thumbnails/18@2x.jpg",
+        alt: "Watermelon Soju",
+		caption: "Watermelon Soju<br />Pocha 32<br />New York, NY"
+    }];
+
+	// http://css-tricks.com/snippets/javascript/shuffle-array/
+    images.sort(function () { return 0.5 - Math.random(); });
+
+	$.each(images, function (index, image) {
+
+        var element = $('<div class="item"><a href="' + image.url + '" data-size="1500x1000" data-med="' + image.mUrl + '" data-med-size="750x500"><img src="' + image.tUrl + '" data-at2x="' + image.tUrl + '" alt="' + image.alt + '" /><figure>' + image.caption + '</figure></a></div>');
+
+        if (index === 0) {
+            element.addClass("active");   
+        }
+
+        element.appendTo("div.my-gallery");
+    });
+
 	$('.my-gallery').owlCarousel({
 		items: 3,
 		itemsDesktop: [1199, 3],
